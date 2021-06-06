b304b08a8b217b255b5ca1be23f0715023ad4ff9
Jacob Zavita <jacobzavita@gmail.com>
2021-06-06 16:59:41 -0700

    one page renders

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const render = require('./lib/render.js')
 const Employee = require('./lib/Employee.js')
 const { prompt } = require('inquirer')
 const path = require("path")
@@ -5,12 +6,12 @@ const { join } = require('path')
 const fs = require("fs")
 const employees = []
 
-const employee1 = new Employee('John', '1234', 'john@email.com', 'Employee')
+// const employee1 = new Employee('John', '1234', 'john@email.com', 'Employee')
 
-console.log(employee1.getName())
-console.log(employee1.getId())
-console.log(employee1.getEmail())
-console.log(employee1.getRole())
+// console.log(employee1.getName())
+// console.log(employee1.getId())
+// console.log(employee1.getEmail())
+// console.log(employee1.getRole())
 
 const createEmployee = () => {
   prompt([
