6c35b115606486a0a250a7bd6a3c95932d167966
Jacob Zavita <jacobzavita@gmail.com>
2021-06-06 11:23:32 -0700

    create test for Employee

---
+++ test/Employee.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@jest/globals')
+const Employee = require('../Employee.js')
+
+test('getName() returns name', () => {
+  const name = new Employee('John')
+  expect(employee.getName()).toBe('John')
+})
+
+test('getId() returns id', () => {
+  const id = new Employee('1234')
+  expect(employee.getId()).toBe('1234')
+})
+
+test('getEmail() returns email', () => {
+  const email = new Employee('john@email.com')
+  expect(employee.getEmail()).toBe('john@email.com')
+})
+
+test('getRole() returns role', () => {
+  const role = new Employee('Employee')
+  expect(employee.getRole()).toBe('Employee')
+})
