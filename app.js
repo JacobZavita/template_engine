82cca01a17b66a47e595d537c603b6c66ed3fce5
Jacob Zavita <jacobzavita@gmail.com>
2021-06-06 11:57:12 -0700

    solve for Employee tests

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const { prompt } = require('inquirer')
 const path = require("path")
 const fs = require("fs")
 
+const employee1 = new Employee('John', '1234', 'john@email.com', 'Employee')
 
-console.log(employee1)
+console.log(employee1.getName())
+console.log(employee1.getId())
+console.log(employee1.getEmail())
+console.log(employee1.getRole())
